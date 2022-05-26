ef76feb854bc89342beb8b663c4c4c9a98033988
sushantkumar2025@gmail.com <sushantkumar2025@gmail.com>
2022-05-26 12:36:33 +0530

    Solution to the dutch national flag problem

---
+++ DutchNationalFlag.java
@@ -0,0 +1,40 @@
+//solution to the problem https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1/
+class DutchNationalFlag
+{
+    public static void sort012(int a[], int n)
+    {
+        
+        
+        int l = 0;
+        int m = 0;
+        int r = a.length-1;
+        
+        while(m <= r){
+            
+            if(a[m] == 0){
+                
+                a[m] = 1;
+                a[l] = 0;
+                l++;
+                m++;
+                
+            }
+            
+            else if(a[m] == 1){
+                
+                m++;
+                
+            }
+            else{
+                
+                a[m] = a[r];
+                a[r] = 2;
+                r--;
+                
+            }
+            
+            
+            
+        }
+    }
+}
